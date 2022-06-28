ffeda10a5807837015f242124fed18764b0fde82
Ezebuka Immaculate Chinomso <105385562+Benytar@users.noreply.github.com>
2022-06-28 15:00:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ if (operator== '+') {
   result = num1 - num2;
   
 }else if (operator=='*') {
+result=num1*num2;  
   
   
-  
-}else{result = num1 / num2;
+}else if (operator=='/'){result = num1 / num2;
      
      
   
